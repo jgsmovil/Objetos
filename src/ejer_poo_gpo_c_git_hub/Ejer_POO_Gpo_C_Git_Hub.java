059b150c750c0c0a893d56d978c32164171fb319
Juana Garcia Seañez <jjggsscia@gmail.com>
2021-04-19 15:50:09 -0600

    Programa despliega saludos, primer ejercicio

---
+++ src/ejer_poo_gpo_c_git_hub/Ejer_POO_Gpo_C_Git_Hub.java
@@ -0,0 +1,20 @@
+/*
+Es un proyecto generado para desarrollar ejercicios
+que se puedan visalizar en Git Hub
+ */
+package ejer_poo_gpo_c_git_hub;
+
+/**
+ *
+ * @author jjggs_000
+ */
+public class Ejer_POO_Gpo_C_Git_Hub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola soy Juanita Garcia");
+    }
+    
+}
